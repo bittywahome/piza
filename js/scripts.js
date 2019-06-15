50f971c31697f8078c857acf04e6389d93194be9
bittywahome <bettywahome253@gmail.com>
2019-06-15 22:27:35 +0300

    add pizza_prices

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-$('.main').click(function() {
-    $(this).children('.hide').slideToggle(1200);
-    $(this).children('.show').toggle(1500);
-   });
+var pizza_Prices=new Array();
+pizza_Prices[bp-sm]=600;
+pizza_Prices[bp-md]=800;
+pizza_Prices[bp-lg]=1000;
+pizza_Prices[vg-sm]=500;
+pizza_Prices[vg-md]=700;
+pizza_Prices[vg-lg]=1050;
+pizza_Prices[pc-lg]=1200;
+pizza_Prices[pc-md]=1050;
+pizza_Prices[pc-sm]=800
+pizza_Prices[bs-md]=700;
+pizza_Prices[bs-lg]=1000;
+pizza_Prices[bs-sm]=500;
+
+functio
+
