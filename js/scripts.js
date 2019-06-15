b0c306900fd83ad19930bc8c57fa8b37122e2c47
bittywahome <bettywahome253@gmail.com>
2019-06-16 00:15:06 +0300

    i have added contents to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var pizza_Prices=new Array();
-pizza_Prices[bp-sm]=600;
-pizza_Prices[bp-md]=800;
-pizza_Prices[bp-lg]=1000;
-pizza_Prices[vg-sm]=500;
-pizza_Prices[vg-md]=700;
+pizza_Prices["bp-sm"]=600;
+pizza_Prices["bp-md"]=800;
+pizza_Prices["bp-lg"]=1000;
+pizza_Prices["vg-sm"]=500;
+pizza_Prices["vg-md"]=700;
 pizza_Prices[vg-lg]=1050;
 pizza_Prices[pc-lg]=1200;
 pizza_Prices[pc-md]=1050;
@@ -17,7 +17,7 @@ function getpizzaPrices()
     var pizzaPrice=0;
     var theForm = document.forms[""];
     var selectedPizza = theForm.elements[""];
-    for (var i=0; i<selectedPizza.clientHeight; i++);
+    for (var i=0; i<12; i++);
 {
     if(selectedpizza [i].checked)
 {
@@ -47,5 +47,10 @@ function getttoppingPrice()
     pizzatoppingPrice= topping_Prices[selectedtopping.value]
     return pizzatoppingPrice;
 }
+
+function getTotal()
+{
+
 var pizzaPrice= getpizzaPrice()+ getttoppingPrice();
-document.getElementById().IinnerHTML= $ + pizzaPrice;
+document.getElementById().innerHTML=  + pizzaPrice;
+}
