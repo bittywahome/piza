e81ae46d5a529ca450a9a5bbb4a3f11134d20a3f
bittywahome <bettywahome253@gmail.com>
2019-06-16 19:03:06 +0300

    add table pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+/* we are going to calculate every one of the inputs when we click one.
+when we click one,its gonna re-calculate all of them at once everytime something is changed.using the each (function()*/
+
+
 $(document).ready(function() {
     $('label').click(function() {
         var total=0;
