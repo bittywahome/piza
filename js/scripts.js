2ac5a504c57a82e71b2cd236ff74c7f2e3f84d82
bittywahome <bettywahome253@gmail.com>
2019-06-17 11:56:06 +0300

    add table contents

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,57 @@
+// Enter values into the invoice table
+function invoice() {
+    var crust= document.querySelector('input[name="crust"]:checked')
+    console.log(crust);
+$(".crust").html(crust.value+" Crust");
+$(crust.value).html("+"+total+"/=");
+
+var size= document.querySelector('input[name="size"]:checked')
+console.log(size);
+$(".size").html(size.value+ "size");
+$(size.value).html("+"+total + "/=");
+
+var toppings= document.querySelector('input[name="toppings"]:checked')
+console.log(toppings);
+$(".toppings").html(toppings.value+ "topping");
+$(toppings.value).html("+" +total +"/=");
+}
+
+(function DELIVERY() {
+    document.getElementById("myForm").style.display="block";
+})
+  ( function closeForm(){
+       document.getElementById("myForm").style.display="none";
+  })
+
+
+
+
+
+
+
+
+
 /* we are going to calculate every one of the inputs when we click one.
-when we click one,its gonna re-calculate all of them at once everytime something is changed.using the each (function()*/
+when we click one,its gonna re-calculate all of them at once everytime something is changed.*/
 
 
 $(document).ready(function() {
+//when you click the label
     $('label').click(function() {
+//its gonna say the total is 0 and for every class that has options we are gonna take the value 
+//and add to it using +=
         var total=0;
         $('.option:checked').each(function(){
+//(this).val() to get the values I had inputed in the html form
             total += parseInt($(this).val()); 
     });
+//calculate total        
     $('#total').html(total + ("/="));
     })
 });
+
+
+
+
+
+
